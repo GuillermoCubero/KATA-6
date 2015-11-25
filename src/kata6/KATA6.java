b8c2ec4ca4a7023a45d65696212c33ccda14c2ed
usuario <usuario@10.22.143.12>
2015-11-25 10:18:22 +0000

    KAta6

--- src/kata6/KATA6.java
+++ src/kata6/KATA6.java
@@ -2,6 +2,7 @@ package kata6;
 
 import static java.awt.PageAttributes.MediaType.A;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import static javax.print.attribute.standard.MediaSize.Engineering.A;
 import static javax.print.attribute.standard.MediaSizeName.A;
@@ -9,13 +10,17 @@ import static javax.swing.text.html.HTML.Tag.A;
 
 public class KATA6 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
         
         String nameFile = "emails.txt";
         
         ArrayList<Person> mailArray = MailListReader.reader(nameFile);
         
+        ArrayList<Person1> lista = PersonLoader.read();
+        
         HistogramBuilder<Person> builder = new HistogramBuilder(mailArray);
+        HistogramBuilder<Person1> builder1 = new HistogramBuilder(lista);
+
         
         Histogram<String> domains = builder.build(new Attribute<Person, String>() {
 
@@ -33,8 +38,27 @@ public class KATA6 {
             }
         });
         
+        Histogram<String> domains1 = builder1.build(new Attribute<Person1, String>(){
+
+            @Override
+            public String get(Person1 item) {
+                return item.getMail().split("@")[1];
+            }
+        
+        });
+        
+        Histogram<Float> pesos = builder1.build(new Attribute<Person1, Float>() {
+
+            @Override
+            public Float get(Person1 item) {
+                return item.getPeso();
+            }
+        });
+        
         new HistogramDisplay(domains, "DOMINIOS").execute();
         new HistogramDisplay(letters, "CARACTER").execute();
+        new HistogramDisplay(domains1, "CARACTER").execute();
+        new HistogramDisplay(pesos, "PESOS").execute();
         System.out.println("Fin");
     }
     
